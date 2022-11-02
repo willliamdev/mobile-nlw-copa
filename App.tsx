9822e23915ebfd1b506809d156ab44671e08b2ef
William Almeida <willliam.dev@gmail.com>
2022-11-02 19:11:17 -0300

    :art: refact app flow

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto"
+import { THEME } from "./src/styles/theme"
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Hello World {":)"}
-      </Text>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
-        style="auto" />
-    </View>
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {
+        fontsLoaded ? <SignIn /> : <Loading />
+      }
+
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24,
-  }
-});
