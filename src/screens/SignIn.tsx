5de8624741250d9139f8a5d942799bdc4902daa4
William Almeida <willliam.dev@gmail.com>
2022-11-03 16:57:22 -0300

    :lipstick: update signin component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,23 @@
 
-import { Center, Text } from "native-base"
+import { Center, Icon, Text } from "native-base"
 
+import { Fontisto } from "@expo/vector-icons"
+
+
+import Logo from "../assets/logo.svg"
+import { Button } from "../components/Button"
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        {/* to do Sign In*/}
-        Sign In
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+        mt={12}
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"}do seu email para criação da sua conta.
       </Text>
     </Center >
   )
